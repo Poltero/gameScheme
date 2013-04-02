b183b36e1f24d256fada02666901e86313eb4023
Pablo Fernandez <pableudo@gmail.com>
2013-04-02 15:03:22 +0200

    gamestates

--- src/main.scm
+++ src/main.scm
@@ -1,6 +1,9 @@
 ;;; Copyright (c) 2012 by Álvaro Castro Castilla
 ;;; Test for Cairo with OpenGL
 
+
+(define-structure world gamestates)
+
 (define (main)
   ((fusion:create-simple-gl-cairo '(width: 1280 height: 752))
    (lambda (event world)
@@ -19,35 +22,45 @@
                       (SDL_KeyboardEvent-keysym kevt))))
            (cond ((= key SDLK_ESCAPE)
                   'exit)
+                 ((= key SDLK_RETURN)
+                  (make-world 'gamescreen))
                  (else
                   (SDL_LogVerbose SDL_LOG_CATEGORY_APPLICATION (string-append "Key: " (number->string key)))
-                  'new-world-modified-by-key-event))))
-        ((= type SDL_WINDOWEVENT)
-         (let* ((wevt (SDL_Event-window event))
-                (event (SDL_WindowEvent-event wevt)))
-           (cond
-            ((= event SDL_WINDOWEVENT_SIZE_CHANGED)
-             (SDL_LogVerbose SDL_LOG_CATEGORY_APPLICATION "Window Size Changed"))
-            ((= event SDL_WINDOWEVENT_RESIZED)
-             (SDL_LogVerbose SDL_LOG_CATEGORY_APPLICATION "Window Resized"))
-            ((= event SDL_WINDOWEVENT_MINIMIZED)
-             (SDL_LogVerbose SDL_LOG_CATEGORY_APPLICATION "Window Minimized"))
-            ((= event SDL_WINDOWEVENT_RESTORED)
-             (SDL_LogVerbose SDL_LOG_CATEGORY_APPLICATION "Window Restored"))))
-         'new-world-modified-by-window-event)
-        ((= type SDL_FINGERDOWN)
-         (SDL_LogInfo SDL_LOG_CATEGORY_APPLICATION "FINGER DOWN!")
-         'new-world-modified-by-finger-event)
+                  world))))
         (else
-         'new-world-modified-by-event))))
-   (let ((posx 80.0))
+         world))))
+   (let ()
      (lambda (cr time world)
        (println (string-append "time: " (object->string time) " ; world: " (object->string world)))
        ;;(SDL_LogInfo SDL_LOG_CATEGORY_APPLICATION (object->string (SDL_GL_Extension_Supported "GL_EXT_texture_format_BGRA8888")))
-       (cairo_set_source_rgba cr 0.0 0.0 0.0 1.0)
-       (cairo_rectangle cr 0.0 0.0 1280.0 752.0)
-       (cairo_fill cr)
-       
-       'new-world-modified-by-draw))
-   'default-world))
 
+       (case (world-gamestates world)
+         
+         ((splashscreen)
+          (cairo_set_source_rgba cr 0.0 0.0 0.0 1.0)
+          (cairo_rectangle cr 0.0 0.0 1280.0 752.0)
+          (cairo_fill cr)
+
+          (cairo_select_font_face cr "Sans" CAIRO_FONT_SLANT_NORMAL CAIRO_FONT_WEIGHT_BOLD)
+          (cairo_set_source_rgba cr 1.0 1.0 1.0 1.0)
+          (cairo_set_font_size cr 90.0)
+          (cairo_move_to cr 240.0 350.0)
+          (cairo_show_text cr "SPLASHSCREEN"))
+
+         ((gamescreen)
+          (cairo_set_source_rgba cr 0.0 0.0 0.0 1.0)
+          (cairo_rectangle cr 0.0 0.0 1280.0 752.0)
+          (cairo_fill cr)
+          
+          (cairo_select_font_face cr "Sans" CAIRO_FONT_SLANT_NORMAL CAIRO_FONT_WEIGHT_BOLD)
+          (cairo_set_source_rgba cr 1.0 1.0 1.0 1.0)
+          (cairo_set_font_size cr 90.0)
+          (cairo_move_to cr 260.0 350.0)
+          (cairo_show_text cr "GAMESCREEN")))
+       
+   
+      
+       
+       world))
+   (make-world 
+    'splashscreen)))

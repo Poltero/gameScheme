36e4a8042b444b8b181b07e64c696099cf5d5735
Poltero <pableudo@gmail.com>
2013-04-10 22:09:01 +0200

    Scrolling auto

--- src/main.scm
+++ src/main.scm
@@ -2,13 +2,23 @@
 ;;; Test for Cairo with OpenGL
 
 (define-structure tile posx posy width height)
+(define-structure camera position)
 (define-structure enemy posx posy width height)
 (define-structure player posx posy width height vstate hstate)
-(define-structure world gamestates tiles player enemy)
+(define-structure world gamestates tiles camera player enemy)
 
 (define map-world '#(#(1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1) ; posicion superior
                      #(1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1) ; posicion medio
                      #(1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1))) ; posicion suelo
+
+;Screen
+(define screen-width 300.0)
+
+;Level dimensions
+(define level-width 30000.0)
+(define level-height 400.0)
+
+
 
 (define collision-tiles
   (lambda (player tileslist)
@@ -46,6 +56,16 @@
               #t
               (loop (cdr rest)))))))
 
+(define update-position-elements
+  (lambda (tiles player camera)
+    (player-posx-set! player (+ (player-posx player) (camera-position camera)))
+    (let loop ((rest tiles))
+      (if (not (null? rest))
+          (begin
+            (tile-posx-set! (car rest) (- (tile-posx  (car rest)) (camera-position camera)))
+            (loop (cdr rest)))
+          '()))))
+
 
 ;; (define collision-down-tile
 ;;   (lambda (player tilelist)
@@ -82,11 +102,13 @@
                       (make-world 
                        'gamescreen 
                        '()
-                       (make-player 
+                       (make-camera
+                        0.0)
+                       (make-player
                         250.0 
                         360.0 
-                        30.0 
-                        30.0 
+                        30.0
+                        30.0
                         'none 
                         'none)
                        (make-enemy
@@ -99,7 +121,8 @@
                   (if (eq? (world-gamestates world) 'gamescreen)
                       (make-world 
                        (world-gamestates world) 
-                       (world-tiles world) 
+                       (world-tiles world)
+                       (world-camera world)
                        (make-player 
                         (player-posx (world-player world)) 
                         (player-posy (world-player world)) 
@@ -114,7 +137,8 @@
                   (if (eq? (world-gamestates world) 'gamescreen)
                       (make-world 
                        (world-gamestates world) 
-                       (world-tiles world) 
+                       (world-tiles world)
+                       (world-camera world)
                        (make-player 
                         (player-posx (world-player world)) 
                         (player-posy (world-player world)) 
@@ -129,7 +153,8 @@
                   (if (eq? (world-gamestates world) 'gamescreen)
                       (make-world 
                        (world-gamestates world) 
-                       (world-tiles world) 
+                       (world-tiles world)
+                       (world-camera world)
                        (make-player 
                         (player-posx (world-player world)) 
                         (player-posy (world-player world)) 
@@ -152,7 +177,8 @@
                   (if (eq? (player-vstate (world-player world)) 'left)
                       (make-world
                        (world-gamestates world) 
-                       (world-tiles world) 
+                       (world-tiles world)
+                       (world-camera world)
                        (make-player 
                         (player-posx (world-player world)) 
                         (player-posy (world-player world)) 
@@ -168,7 +194,8 @@
                   (if (eq? (player-vstate (world-player world)) 'right)
                       (make-world 
                        (world-gamestates world) 
-                       (world-tiles world) 
+                       (world-tiles world)
+                       (world-camera world)
                        (make-player 
                         (player-posx (world-player world)) 
                         (player-posy (world-player world)) 
@@ -183,7 +210,8 @@
                   (if (eq? (world-gamestates world) 'gamescreen)
                       (make-world 
                        (world-gamestates world) 
-                       (world-tiles world) 
+                       (world-tiles world)
+                       (world-camera world)
                        (make-player 
                         (player-posx (world-player world)) 
                         (player-posy (world-player world)) 
@@ -203,7 +231,7 @@
      (lambda (cr time world)
        (set! delta-time (- time last-time))
        (set! last-time time)
-      ;(println (string-append "time: " (object->string time) " ; world: " (object->string world)))
+       ;(println (string-append "time: " (object->string time) " ; world: " (object->string ( world-tiles world))))
        ;;(SDL_LogInfo SDL_LOG_CATEGORY_APPLICATION (object->string (SDL_GL_Extension_Supported "GL_EXT_texture_format_BGRA8888")))
 
        (case (world-gamestates world)
@@ -236,10 +264,10 @@
           ;;calculate tiles in the world and paint
           
           (cairo_set_source_rgba cr 1.0 1.0 1.0 1.0)
-          (let loop ((count-tiles (/ 1280 20)) (posx 0.0))
+          (let loop ((count-tiles (/ 5000 20)) (posx 0.0))
             (if (= count-tiles 0)
                 '()
-                (begin (cairo_rectangle cr posx 400.0 20.0 20.0)
+                (begin (cairo_rectangle cr (- posx (camera-position (world-camera world))) 400.0 20.0 20.0)
                        (cairo_fill cr)
                        (loop (- count-tiles 1) (+ posx 20.0)))))
 
@@ -282,21 +310,35 @@
           (let loop ((rest (world-tiles world)))
             (if (not (null? rest))
                 (begin
-                  (tile-posx-set! (car rest) (- (tile-posx (car rest)) (* 0.09 delta-time)))
-                  ;(tile-posx-set! (car rest) 500.0)
-                  (cairo_rectangle cr (tile-posx (car rest)) (tile-posy (car rest)) (tile-width (car rest)) (tile-height (car rest)))
+                  (cairo_rectangle cr (- (tile-posx  (car rest)) (camera-position (world-camera world))) (tile-posy (car rest)) (tile-width (car rest)) (tile-height (car rest)))
+                  (cairo_fill cr)
                   (loop (cdr rest)))
                 '()))
+          
 
 
 
           (if (eq? (player-vstate (world-player world)) 'left)
-             (let player-left ((player (world-player world)))
-               (player-posx-set! player (- (player-posx player) (* 0.3 delta-time)))))
+              (let player-left ((camera (world-camera world)) (tiles (world-tiles world)) (player (world-player world)))
+               (begin
+                 ;(camera-position-set! camera (- (camera-position camera) (* 0.3 delta-time)))
+                 (player-posx-set! player (- (player-posx player) (* 0.3 delta-time)))
+                 ;(update-position-elements tiles player camera)
+                 )
+               ))
 
           (if (eq? (player-vstate (world-player world)) 'right)
-             (let player-right ((player (world-player world)))
-               (player-posx-set! player (+ (player-posx player) (* 0.3 delta-time)))))
+             (let player-right ((camera (world-camera world)) (tiles (world-tiles world)) (player (world-player world)))
+               (begin
+                 (player-posx-set! player (+ (player-posx player) (* 0.3 delta-time)))
+                 ;(camera-position-set! camera (+ (camera-position camera) (* 0.3 delta-time)))
+                 ;(update-position-elements tiles player camera)
+                 )))
+
+          ;;Control limits X
+          ;; (if (or (< (player-posx (world-player world)) 0) (> (+ (player-posx (world-player world)) (player-width (world-player world))) level-width))
+          ;;     (player-posx-set! (world-player world) (+ (player-posx (world-player world)) (* 0.3 delta-time))))
+
           
 
           (if (and (eq? (player-hstate (world-player world)) 'up) (not (collision-top-tiles (world-player world) (world-tiles world))))
@@ -307,26 +349,56 @@
 
           (if (and (eq? (player-hstate (world-player world)) 'down) (not (collision-down-tiles (world-player world) (world-tiles world))))
               (if (and (< (player-posy (world-player world)) 370))
-                 (let player-down ((player (world-player world)))
+                  (let player-down ((player (world-player world)))
                    (player-posy-set! player (+ (player-posy player) (* 0.3 delta-time))))
                  ;(player-posy-set! (world-player world) (+ (player-posy (world-player world)) 20))
-                ))
+                 ))
+
+          ;;Control limits Y
+          (if (or (< (player-posy (world-player world)) 0) (> (+ (player-posy (world-player world)) (player-height (world-player world))) level-height))
+              (player-posy-set! (world-player world) (- (player-posy (world-player world)) (* 0.3 delta-time))))
+          
+
+          ;;Set camera
+          
+          ;;Center the camera over the player
+          ;(camera-position-set! (world-camera world) (- (/ (+ (player-posx (world-player world)) (player-width (world-player world))) 2) (/ screen-width 2.0)))
+          (camera-position-set! (world-camera world ) (+ (camera-position (world-camera world)) (* 0.1 delta-time)))
+
+          ;;keep the camera in bounds
+          (if (< (camera-position (world-camera world)) 0)
+              (camera-position-set! (world-camera world) 0))
+          (if (> (camera-position (world-camera world)) (- level-width (camera-position (world-camera world))))
+              (camera-position-set! (world-camera world) (- level-width (camera-position (world-camera world)))))
 
 
-          (if (collision-down-tiles (world-player world) (world-tiles world))
-              (println "Collision Down!"))
+          ;; (if (> (player-posx (world-player world)) screen-width)
+          ;;    (camera-position-set! (world-camera world) (- screen-width (camera-position (world-camera world)))))
           
           
+          (println (object->string (player-posx (world-player world))))
 
-          (if (and (not(eq? (player-hstate (world-player world)) 'left)) (not (eq? (player-hstate (world-player world)) 'right)))
-              (let decrement-position-player ((player (world-player world)))
-                (player-posx-set! player (- (player-posx player) (* 0.09 delta-time)))))
+          ;; (if (collision-down-tiles (world-player world) (world-tiles world))
+          ;;     (println "Collision Down!"))
+          
+          
+          ;; (if (> (+ (player-posx (world-player world)) (camera-position (world-camera world))) 800)
+          ;;     (begin
+          ;;       (camera-position-set! (world-camera world) (- (camera-position (world-camera world)) (* 0.5 delta-time)))
+          ;;       )
+          ;;     (println "Posx: " (object->string (player-posx (world-player world)))))
+
+
+          ;; (if (and (not(eq? (player-hstate (world-player world)) 'left)) (not (eq? (player-hstate (world-player world)) 'right)))
+          ;;     (let decrement-position-player ((player (world-player world)))
+          ;;       (player-posx-set! player (- (player-posx player) (* 0.09 delta-time)))))
 
 
           ;; Drawing player
-          (cairo_set_source_rgba cr 1.0 1.0 1.0 1.0)
+          (cairo_set_source_rgba cr 1.0 0.0 0.0 1.0)
           (let drawing-player ((player (world-player world)))
-            (cairo_rectangle cr (player-posx player) (player-posy player) (player-width player) (player-height player)))
+            (cairo_rectangle cr (- ( player-posx player) (camera-position (world-camera world))) (player-posy player) (player-width player) (player-height player)))
+          
           (cairo_fill cr)
           
           ;; (cairo_rectangle cr (+ 250.0 20) 360.0 10.0 10.0)
@@ -335,10 +407,10 @@
 
 
           ;; Drawing enemy principal
-          (cairo_set_source_rgba cr 1.0 0.0 0.0 1.0)
-          (let drawing-enemy-principal ((enemy (world-enemy world)))
-            (cairo_rectangle cr (enemy-posx enemy) (enemy-posy enemy) (enemy-width enemy) (enemy-height enemy)))
-          (cairo_fill cr)
+          ;; (cairo_set_source_rgba cr 1.0 0.0 0.0 1.0)
+          ;; (let drawing-enemy-principal ((enemy (world-enemy world)))
+          ;;   (cairo_rectangle cr (enemy-posx enemy) (enemy-posy enemy) (enemy-width enemy) (enemy-height enemy)))
+          ;; (cairo_fill cr)
           
           ))
        
@@ -349,5 +421,6 @@
    (make-world 
     'splashscreen
     '()
+    'none
     'none
     'none)))
